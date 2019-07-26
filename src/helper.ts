2723d3b248885667827f38078456ff1010978d22
Patrick Przystolik <mtcllf@gmail.com>
2019-07-26 09:10:24 +0200

    #1: color definitions

--- src/helper.ts
+++ src/helper.ts
@@ -14,6 +14,34 @@ export interface mystruct {
   routes: string[];
 }
 
+export type TOs = 'BSD'|'BBS'|'IBM'|'MIL'|'Dynix'|'ATT'|'Ultrix'|'VMS'|'SunOS'|'HP-UX'|'Xenix'|'SysV'|'AIX'|'MACH'|'AUX'|'OSES'|'WOPR'|'SECOS'|'RELIC'|'TEL/OS'|'ENCOM';
+export interface polygonStruct { os: TOs, color: string, shape: string | null }
+
+// Colornames in Sub Section SVG: https://www.graphviz.org/doc/info/colors.html
+export const polygons: polygonStruct[] = [
+  {os: 'BSD', color: 'indianred', shape: null},
+  {os: 'BBS', color: 'ivory', shape: null},
+  {os: 'IBM', color: 'lightskyblue', shape: null},
+  {os: 'MIL', color: 'magenta', shape: null},
+  {os: 'Dynix', color: 'yellowgreen', shape: null},
+  {os: 'ATT', color: 'wheat', shape: null},
+  {os: 'Ultrix', color: 'darkturquoise', shape: null},
+  {os: 'VMS', color: 'silver', shape: null},
+  {os: 'SunOS', color: 'gold', shape: null},
+  {os: 'HP-UX', color: 'burlywood', shape: null},
+  {os: 'Xenix', color: 'paleturquoise', shape: null},
+  {os: 'SysV', color: 'cyan', shape: null},
+  {os: 'AIX', color: 'greenyellow', shape: null},
+  {os: 'MACH', color: 'moccasin', shape: null},
+  {os: 'AUX', color: 'palegreen', shape: null},
+  {os: 'OSES', color: 'lightsteelblue', shape: null},
+  {os: 'WOPR', color: 'crimson', shape: null},
+  {os: 'SECOS', color: 'fuchsia', shape: null},
+  {os: 'RELIC', color: 'deeppink', shape: null},
+  {os: 'TEL/OS', color: 'lawngreen', shape: null},
+  {os: 'ENCOM', color: 'purple', shape: null}
+];
+
 export const UNKNOWNHOST = `[UNKNOWN_HOST_%%NUM%%]`;
 export const NONETWORKCONNECTIONS = `[NO_NETWORK_CONNECTIONS]`;
 
@@ -40,5 +68,9 @@ export class Helper {
 
   public writeOut(filename: string, data: string): void {
     fs.appendFileSync(filename, data);
+  }
+
+  public getColorShape(os: TOs): polygonStruct | undefined {
+    return polygons.find(poly => poly.os === os);
   }
 }
