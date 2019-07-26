543191df237838e23b4596d67d78301ee59d9d7f
Patrick Przystolik <mtcllf@gmail.com>
2019-07-26 08:17:47 +0200

    adjusted compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
 then
-  echo "Usage: $0 <input-dot-file> <svg-output-file> <Tsvg|...>"
+  echo "Usage: $0 <input-dot-file> <output-file> <-Tsvg|...>"
   exit 1
 fi
 
 time neato -x -Goverlap=scale $3 $1 -o $2 -v
+
